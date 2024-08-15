b2a651b5c6304e179bee41955cbfe37babbb4b83
restim89 <restim89@gmail.com>
2024-08-16 00:55:07 +0300

    Bathhouse

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-54-gigapixel-cgi-2x-",
+      "name": "54-gigapixel-cgi-2x копия",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 0.977971444747918
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08478228357251183,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "1-54-gigapixel-cgi-2x"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-54-gigapixel-cgi-2x",
+      "name": "54-gigapixel-cgi-2x",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.091964922279754,
+          "pitch": 0.033972709566016945,
+          "rotation": 0,
+          "target": "0-54-gigapixel-cgi-2x-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Bathhouse",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
